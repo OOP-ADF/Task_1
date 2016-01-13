d3aad6990f3225dde73538858b9b40e731abb75d
agungrbudiman <agungrbudiman@gmail.com>
2016-01-13 12:21:04 +0700

    2

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,33 @@
+/*
+NAMA 	: Agung Rahmat Budiman
+NIM		: 1301144142
+Kelas	: IF-38-02
+*/
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+		int angka;
+		boolean prima=true;
+		Scanner input = new Scanner(System.in);
+		System.out.print("Inputkan angka : ");
+		angka = input.nextInt();
+		if(angka>1) {
+			for (int i=2;i<angka;i++) {
+				if((angka % i)==0) {
+					prima=false;
+				}
+			}
+			if(prima==true || angka==2) {
+				System.out.print("Bilangan prima");
+			}	
+			else {
+				System.out.print("Bukan bilangan prima");
+			}
+		}
+		else {
+			System.out.print("Bukan bilangan prima");
+		}
+	}
+}
