e2e7cee9e51abed101401e300f4077a77c6b9b9d
Hafidrozaq <syahrizalrozaqi@gmail.com>
2016-01-14 05:59:58 +0700

    final

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,42 @@
-
+//Syahrizal Hafid Rozaqi - IF3802 - 1301144252
+import java.util.Scanner;
+public class IndexPoint {
+    public static void main(String[] args) {
+        System.out.println("Menghitung Nilai Akhir");
+        System.out.print("Masukkan Nilai Quiz : ");
+        Scanner quiz = new Scanner(System.in);
+        float nilaiquiz = quiz.nextFloat();
+        System.out.print("Masukkan Nilai UTS : ");
+        Scanner uts = new Scanner(System.in);
+        float nilaiuts = uts.nextFloat();
+        System.out.print("Masukkan Nilai UAS : ");
+        Scanner uas = new Scanner(System.in);
+        float nilaiuas = uas.nextFloat();
+        
+        float hasilquiz = ((25*nilaiquiz)/100);
+        float hasiluts = ((35*nilaiuts)/100);
+        float hasiluas = ((40*nilaiuas)/100);
+        float total = (hasilquiz+hasiluts+hasiluas);
+        
+        if (total >= 85){
+            String grade = "Excellent";
+            System.out.println("Anda memiliki Index Point "+total+" Dengan Grade = "+grade);
+        }
+        else if (total >= 75){
+            String grade = "Very Good";
+            System.out.println("Anda memiliki Index Point "+total+" Dengan Grade = "+grade);
+        }
+        else if (total >= 65){
+            String grade = "Good";
+            System.out.println("Anda memiliki Index Point "+total+" Dengan Grade = "+grade);
+        }
+        else if (total >= 50){
+            String grade = "Accepted";
+            System.out.println("Anda memiliki Index Point "+total+" Dengan Grade = "+grade);
+        }
+        else if (total >= 0){
+            String grade = "Failed";
+            System.out.println("Anda memiliki Index Point "+total+" Dengan Grade = "+grade);
+        }
+    }
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,24 @@
+//Syahrizal Hafid Rozaqi - IF3802 - 1301144252
+import java.util.Scanner;
+public class PrimeNumber {   
+public static void main(String[] args) {  
 
+Scanner input = new Scanner (System.in);  
+System.out.println("Masukan bilangan :");  
+
+int bil = input.nextInt();  
+boolean prima = true;  
+
+for (int i=2; i<bil; i++){        
+	if ((bil % i) == 0) {  
+		prima = false; break;  
+  
+    }  
+}  
+
+if (prima)    
+	System.out.println(bil + " adalah bilangan PRIMA ");    
+else  
+	System.out.println(bil + " adalah bukan bilangan PRIMA ");  
+    }  
+}  
