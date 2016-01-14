0668d9ee120434250fa81c2696e61f2f60710cb2
yulizar <erwin_lizar@live.com>
2016-01-14 22:30:56 +0700

    Task Completed

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,21 @@
+import java.util.Scanner;
 
+public class PrimeNumber{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Masukkan sebuah bilangan : ");
+        int bilangan = input.nextInt();
+        
+        if (bilangan ==1 ){
+            System.out.println("Bukan bilangan prima");
+        }else if ((bilangan ==2) || (bilangan == 3) || (bilangan ==5) || (bilangan ==7)){
+            System.out.println("Bilangan prima");
+        }
+        else if ((bilangan % 2 == 0) || (bilangan % 3 == 0) || (bilangan % 5 == 0) || (bilangan % 7==0)){
+            System.out.println("Bukan Bilangan prima");
+        } else{
+            System.out.println("Bilangan prima");
+        }
+    }
+}
---
+++ TemperatureConvert.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class TemperatureConvert{
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int k;
+        double f,r;
+        
+        System.out.print("Masukkan temperature dalam celcius : ");
+        int temperature = input.nextInt();
+        f = (9 * temperature/5) +32;
+        r = (4 * temperature/5);
+        k = temperature + 273;
+        
+        System.out.println("Nilai fahrenheit dari input tadi : "+f);
+        System.out.println("Nilai reamur dari input tadi : "+r);
+        System.out.println("Niali Kelvin dari input tadi : "+k);
+    }
+}
