5a6be6ee1c1c52f3f15d3469c56d67f395409d5f
hanidika <hanidikar02@gmail.com>
2016-01-14 21:13:53 +0700

    Update TemperatureConvert.java

--- TemperatureConvert.java
+++ TemperatureConvert.java
@@ -6,19 +6,23 @@
 
 
 import java.util.Scanner;
-public class TemparatureConvert {
-    public static void main(String[] args) {
+public class JavaApplication4 {
+
+   public static void main(String[] args) {
         double suhu, hasil;
         int pilih;
+        char ulang;
         
         Scanner nilai = new Scanner(System.in);
-        System.out.println("MENU");
-        System.out.println("1. CONVERT TO REAMUR");
-        System.out.println("2. CONVERT TO FAHRENHEIT");
-        System.out.println("3. CONVERT TO KELVIN");
-        System.out.println();
-        System.out.print("Pilih: ");
-        pilih=nilai.nextInt();
+        Scanner nilai2 = new Scanner(System.in);
+        do {
+         System.out.println("MENU");
+         System.out.println("1. CONVERT TO REAMUR");
+         System.out.println("2. CONVERT TO FAHRENHEIT");
+         System.out.println("3. CONVERT TO KELVIN");
+         System.out.println();
+         System.out.print("Pilih: ");
+         pilih=nilai.nextInt();
         
         switch(pilih) {
             case 1:
@@ -40,6 +44,11 @@ public class TemparatureConvert {
                 System.out.print("Hasil: "+hasil+" Kelvin");
                 break;
         }
+        System.out.println();
+        System.out.println("Ingin merubah ke suhu lainnya ? (y/n)");
+        System.out.print("Pilih : ");
+        ulang=nilai2.next().charAt(0);
+        System.out.println();
+      }while (ulang != 'n' );
     }
-    
 }
