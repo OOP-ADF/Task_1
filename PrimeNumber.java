499419beda47ce261cce88e778feb7328b6abf6e
mhrdandika <mhrdandika@yahoo.com>
2016-01-15 19:13:52 +0700

    up

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,30 @@
+// botak
+import java.util.Scanner;
 
+public class PrimeNumber {
+    
+    public static boolean isPrime(int num) {
+        int to = (int) Math.sqrt(num) + 1;
+        
+        for (int i = 2; i <= to; i++) {
+            if (num % i == 0) {
+                return false;                
+            }
+        }
+        return num > 1 && true;
+    }
+       
+    public static void main(String[] args) {
+        
+       Scanner sc = new Scanner(System.in);
+       
+       int num = sc.nextInt();
+       
+       if (isPrime(num)){
+           System.out.println("this number is prime");
+       } else {
+           System.out.println("This number is not prime");
+       }
+ 
+    }
+}
