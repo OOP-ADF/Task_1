bc7b0d7a5e0de7bf5bd01f9079d0773532c943fe
Muhammad Yuslan Abubakar <yuslan.black@gmail.com>
2016-01-15 10:19:06 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,26 @@
+package primenumber;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author yuslanabubakar
+ */
+public class PrimeNumber {
+    public static void main(String[] args) {               
+        System.out.println("Selamat Datang Di Program Bilangan Prima");
+        System.out.println();
+        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Angka : ");
+        int angka = input.nextInt();
+        
+        if (angka % 2 == 1) {
+            System.out.println("Angka "+angka+" adalah bilangan prima");
+        }
+        else {
+            System.out.println("Angka "+angka+" adalah bukan bilangan prima");
+        }
+    }
+    
+}
