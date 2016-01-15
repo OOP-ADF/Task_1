906e522837ac6a3b10d91fa0ce6ea36cb025a77a
Thoriq Abdul Aziz <riq.aziz@gmail.com>
2016-01-15 17:23:50 +0700

    Update PrimeNumber.java
    Thoriq Abdul Aziz Muqodas
    1301144341
    IF-38-01

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,23 @@
-
+/* Thoriq Abdul Aziz Muqodas
+	1301144341
+	IF-38-01
+*/
+import java.util.Scanner;
+public class PrimeNumber{
+	
+	public static void main(String[]args){
+		Scanner check = new  Scanner(System.in);
+		System.out.print("Masukan Angka : ");
+		int x = check.nextInt();
+		int j = 0;
+		for(int i=2;i<x;i++){
+			if(x%i==0){
+				j++;
+			}
+		}
+		if(j==0){
+			System.out.println(x+" adalah bilangan Prima");
+		}
+		else{System.out.println(x+" bukan bilangan Prima");}
+	}
+}
