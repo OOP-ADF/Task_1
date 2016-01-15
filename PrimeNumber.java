cd7658f5fa71c87a33592bbfc38e3c87df5316b9
dzakytafz <dzack_danield@yahoo.com>
2016-01-15 15:59:15 +0700

    Tugas 1 PBO
    NAMA  : DZAKYTA AFUZAGANI
    NIM     : 1301140122
    KELAS   : IF 38 02

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,26 @@
+// NAMA : DZAKYTA AFUZAGANI
+// NIM	: 1301140122
+// KELAS: IF 38 02
+
+import java.util.Scanner;
+public class PrimeNumber{
+	public static void main(String[] args){
+		double bil,counter;
+		counter = 0;
+		Scanner input=new Scanner(System.in);
+		System.out.print("Bilangan yang di input : ");
+		bil=input.nextDouble();
+		for (int i=1; i<=bil ; i++){
+			if(bil%i == 0){
+				counter++;
+			}
+		}
+		if(counter == 2){
+		System.out.print("Bilangan yang anda input PRIMA");
+		}
+		else{
+		System.out.print("Bilangan yang anda input BUKAN PRIMA");
+		}
+	}
+}
 
