ede49187feee2810ecddff74fd4c7002aaaa94f5
Aryaditya-1301144162 <aryadtya09@gmail.com>
2016-01-16 11:22:13 +0700

    Aryaditya H.p Tugas PBO 1
    Aryaditya H.p - 1301144162 - IF3802

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,39 @@
+//aryaditya H.p - 1301144162 - IF3802
 
+import java.util.Scanner;
+public class IndexPoint {
+ 	public static void main(String[] args){
+ 
+        double uas, uts, quiz, nilaiakhir ;
+ 		
+ 		Scanner input = new Scanner(System.in);
+ 		
+ 		System.out.print("Masukkan Nilai UAS: ");
+ 		uas = input.nextDouble();
+ 		
+ 		System.out.print("Masukkan Nilai UTS: ");
+ 		uts = input.nextDouble();
+ 		
+ 		System.out.print("Masukkan Nilai QUIZ: ");
+ 		quiz = input.nextDouble();
+ 	
+ 		nilaiakhir = (0.35 * uts)+(0.4 * uas)+(0.25 * quiz); 
+                 
+                 if (nilaiakhir>85) {
+                     System.out.print("Excellent");
+                 }
+                 else if (nilaiakhir>75) {
+                     System.out.print("Very Good");    
+                 }
+                 else if (nilaiakhir>65) {
+                     System.out.print("Good");    
+                 }
+                else if (nilaiakhir>50) {
+                     System.out.print("Average");    
+                 }
+                 else if (nilaiakhir<50) {
+                     System.out.print("Fail");    
+                 }
+                 
+     }
+ }
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,17 @@
+//aryaditya H.p - 1301144162 - IF3802
 
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.printf("Masukkan Bilangan : ");
+        int n=s.nextInt();
+        int b = 0;
+        for(int i=1;i<=n;i++){
+        if(n%i==0)
+            b=b+1;  }
+        if(b==2)
+            System.out.printf("Bilangan adalah bil.prima ");
+        else
+            System.out.printf("Bilangan bukan bil.prima ");
+    }
