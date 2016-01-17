3cebfe1707558fc99d2a4c4da13bb5f671c51ba9
agustiananggi <anggi.agustian.sutisna@gmail.com>
2016-01-17 22:37:34 +0700

    Update IndexPoint.java

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,39 +1,36 @@
 //Anggi Agustian - 1301144149
-
 import java.util.Scanner;
+
 public class IndexPoint {
- 	public static void main(String[] args){
 
-        double uas, uts, quiz, nilaiakhir ;
+    public static void main(String[] args) {
 
- 		Scanner input = new Scanner(System.in);
+        double uas, uts, quiz, nilaiakhir;
 
- 		System.out.print("Masukkan Nilai UAS : ");
- 		uas = input.nextDouble();
+        Scanner input = new Scanner(System.in);
 
- 		System.out.print("Masukkan Nilai UTS : ");
- 		uts = input.nextDouble();
+        System.out.print("Masukkan Nilai UAS : ");
+        uas = input.nextDouble();
 
- 		System.out.print("Masukkan Nilai QUIZ : ");
- 		quiz = input.nextDouble();
+        System.out.print("Masukkan Nilai UTS : ");
+        uts = input.nextDouble();
 
- 		nilaiakhir = (0.35 * uts)+(0.4 * uas)+(0.25 * quiz);
+        System.out.print("Masukkan Nilai QUIZ : ");
+        quiz = input.nextDouble();
 
-                 if (nilaiakhir>85) {
-                     System.out.print(" Grade : Excellent");
-                 }
-                 else if (nilaiakhir>75) {
-                     System.out.print("Grade : Very Good");
-                 }
-                 else if (nilaiakhir>65) {
-                     System.out.print("Grade : Good");
-                 }
-                else if (nilaiakhir>50) {
-                     System.out.print("Grade : Average");
-                 }
-                 else if (nilaiakhir<50) {
-                     System.out.print("Grade : Failed");
-                 }
+        nilaiakhir = (0.35 * uts) + (0.4 * uas) + (0.25 * quiz);
 
-     }
- }
+        if (nilaiakhir > 85) {
+            System.out.println(" Grade : Excellent");
+        } else if (nilaiakhir > 75) {
+            System.out.println("Grade : Very Good");
+        } else if (nilaiakhir > 65) {
+            System.out.println("Grade : Good");
+        } else if (nilaiakhir > 50) {
+            System.out.println("Grade : Average");
+        } else if (nilaiakhir < 50) {
+            System.out.println("Grade : Failed");
+        }
+
+    }
+}
