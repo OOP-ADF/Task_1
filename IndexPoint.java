c1b936960b6bdb028acbbdab041ed775cbf4af88
andiakhmadf <agile678@gmail.com>
2016-01-17 13:17:11 +0700

    [IF-38-09] Andi Akhmad Fauzi 1301144009

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,39 @@
-
+package indexpoint;
+import java.util.Scanner;
+/**
+ *
+ * @author Andi Akhmad Fauzi
+ */
+/* Andi Akhmad Fauzi IF-38-09 1301144009 */
+public class IndexPoint {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        float mid;
+        float fin;
+        float quiz;
+        double tot;
+        Scanner x = new Scanner(System.in); // Scanner Float
+        System.out.println("=== INDEX POINT CALCULATOR ===");
+        System.out.print("Input Mid-Term (UTS) Score  : "); mid = x.nextFloat();
+        System.out.print("Input Final test (UAS) Score  : "); fin = x.nextFloat();
+        System.out.print("Input Quiz Score : "); quiz = x.nextFloat();
+        
+        tot = ((0.35*mid) + (0.4*fin) + (0.25*quiz));
+        System.out.println("Your total Score is: " + tot);
+        if (tot >= 85 && tot <= 100)
+             System.out.println("Your Grade is Excellent");
+        else if (tot >= 75 && tot < 85)
+             System.out.println("Your Grade is Very Good");
+        else if (tot >= 65 && tot < 75)
+             System.out.println("Your Grade is Good");
+        else if (tot >= 50 && tot < 65)
+             System.out.println("Your Grade is Accepted");
+        else if (tot >= 0 && tot < 50)
+             System.out.println("You Are Failed");     
+    }
+    
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,35 @@
+package primenumber;
+import java.util.Scanner;
+/**
+ *
+ * @author Andi Akhmad Fauzi
+ */
+ 
+/* Andi Akhmad Fauzi IF-38-09 1301144009 */
+public class PrimeNumber {
 
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int inp;
+		int x;
+		int i;
+		x=0;
+		System.out.println("===PRIME NUMBER PROGRAM===");
+		Scanner s = new Scanner(System.in);
+		System.out.print("Masukkan bilangan: "); 
+		inp = s.nextInt();
+		
+		for (i=1; i<=inp; i++) 
+	    { if (inp % i==0)
+				x=x+1;
+		}
+		
+		if (x==2) 
+			System.out.println("Bilangan tersebut merupakan bilangan prima");
+		else if (x!=2)
+			System.out.println("Bilangan tersebut bukan bilangan prima");
+    
+}
+}
