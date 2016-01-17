62eed460034b90c8ef9bd6ea6c1bd7a2a9fd7715
Aryaditya-1301144162 <aryadtya09@gmail.com>
2016-01-17 21:23:47 +0700

    Hariz M.Wibawa Tugas PBO 1
    Hariz M.Wibawa
    1301144119
    IF3809

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,17 @@
 
+//Hariz Mulya W - 1301144119 - IF - 38 - 09
+import java.util.Scanner;
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner s=new Scanner(System.in);
+        System.out.printf("Masukkan Bilangan : ");
+        int n=s.nextInt();
+        int b = 0;
+        for(int i=1;i<=n;i++){
+        if(n%i==0)
+            b=b+1;  }
+        if(b==2)
+            System.out.printf("Bilangan adalah bil.prima ");
+        else
+            System.out.printf("Bilangan bukan bil.prima ");
+    }
