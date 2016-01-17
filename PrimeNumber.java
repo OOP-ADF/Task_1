658c4e9ba662a1899e1bcd92842a64698d4dfd66
ZulfaUmi <ulfawardanii@gmail.com>
2016-01-17 16:26:02 +0700

    Task_1
    Task_1

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,39 @@
+/*
+Nama: ZULFA UMI WARDANI
+Nim: 1301144082
+Kelas: IF-38-02
+*/
 
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author ZULFA UMI WARDANI
+ */
+public class PrimeNumber{
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Aplikasi Cek Bilangan Prima");
+        System.out.print("Angka : ");
+        Scanner scan = new Scanner(System.in);
+        int input = scan.nextInt();
+        
+        int temp=0;
+        for (int i=1;i<input;i++){
+            if (input%i==0){
+                temp=temp+1;
+            }
+        }
+        if (temp==1){
+            System.out.println("Prime Number");
+        }else{
+            System.out.println("NOT Prime Number");
+        }
+    }
+    
+}
