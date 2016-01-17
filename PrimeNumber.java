4d09c621cb209c38b33b51f1fa53a8a490fb79cc
agustiananggi <anggi.agustian.sutisna@gmail.com>
2016-01-17 22:31:08 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,27 @@
+//Anggi Agustian - 1301144149
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Bilangan : ");
+        int bilangan = input.nextInt();
+        int b = 0;
+        if (bilangan == 1) {
+            System.out.print("Bilangan tersebut adalah Bilangan PRIMA ");
+        } else {
+            for (int i = 1; i <= bilangan; i++) {
+                if (bilangan % i == 0) {
+                    b = b + 1;
+                }
+            }
+            if (b == 2) {
+                System.out.print("Bilangan tersebut adalah Bilangan PRIMA ");
+            } else {
+                System.out.print("Bilangan tersebut bukanlah Bilangan PRIMA ");
+            }
+        }
+    }
+}
 
