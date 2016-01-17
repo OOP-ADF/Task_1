e7f711b24f2a55d855abf3ca35c4d4a51b06300d
gctatmaja <genturcta@gmail.com>
2016-01-18 01:53:44 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,48 @@
+/*
+ * Nama     : GENTUR CIPTO TRI ATMAJA
+ * NIM      : 1301140239
+ * KELAS    : IF 38-09
+ */
+package primenumber;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author GENTUR CIPTO TRI ATMAJA
+ */
+public class PrimeNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+   
+    int bil;
+    int hasil;
+    
+    Scanner scan= new Scanner(System.in);
+    System.out.println("Masukkan Bilangan yang akan di periksa : ");
+    
+    bil=scan.nextInt();
+    
+    hasil=0;
+        for (int i = 1; i<=bil ; i++) 
+        {
+            if(bil%i==0)
+            {
+                hasil=hasil+1;
+            } 
+        }
+        if(hasil==2)
+        {
+            System.out.println("merupakan sebuah bilangan Prima");
+        }
+        else
+        {
+            System.out.println("Tidak termasuk bilangan Prima");
+        }
+    }
+}
+    
 
