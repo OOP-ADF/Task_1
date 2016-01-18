13147d797cfad17df06a3f56944cfaeb7d8f8b24
aristyawirawan <wirawanaristya@gmail.com>
2016-01-18 22:21:31 +0700

    1103134329

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,4 +1,36 @@
-fgdhhg
-yfjfjf
-kll
-bjk
+//Nama    : Aristya Wirawan
+//NIM     : 1103134329
+//Kelas   : IF-38-09
+
+import java.util.Scanner;
+public class IndexPoint{
+	private static void main(String[] args){
+		Scanner input = new Scanner(System.in);
+		
+		String nama;
+		double nUts, nUas, nQuiz, nAkhir;
+		System.out.print("Masukkan Nama : ");
+		nama = input.nextLine();
+		System.out.print("Nilai UTS : ");
+		nUts = input.nextDouble();
+		System.out.print("Nilai UAS : ");
+		nUas = input.nextDouble();
+		System.out.print("Nilai Quiz : ");
+		nQuiz = input.nextDouble();
+		
+		nAkhir = ((nUts*0.35)+(nUas*0.4)+(nQuiz*0.2));
+		System.out.println("Nilai Akhir yang diperoleh : "+nAkhir);
+		if (nAkhir<50){
+			System.out.println("Grade : Failed");
+		}else if(nAkhir<65){
+			System.out.println("Grade : Accepted");
+		}else if(nAkhir<75){
+			System.out.println("Grade : Good");
+		}else if(nAkhir<85){
+			System.out.println("Grade : Very Good");
+		}else{
+			System.out.println("Grade : Excellent");
+		}
+		
+	}
+}
