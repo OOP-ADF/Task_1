c3387c615580f3c21e181286498b2bfed638bf3a
damarradistya <damaradistya@gmail.com>
2016-01-18 10:07:46 +0700

    1103120164
    IndexPoint & Prime number

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,44 @@
+/*
+ Damar Radistya
+ 1103120164
+ IF-38-09
+ */
 
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static boolean Prime(int num) {
+        for (int i = 2; i <= num/2; i ++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        int num;
+        String check = "y";
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Prime Number Checker");
+
+        while ("y".equals(check)) {
+
+            System.out.print("\nInput number : ");
+
+            num = s.nextInt();
+
+            if (Prime(num)) {
+                System.out.println(num + " is a prime number");
+            } else {
+                System.out.println(num + " is not a prime number");
+            }
+            System.out.print("\nCheck Again? (y/n) :");
+            check = s.next();
+        }
+    }
+}
