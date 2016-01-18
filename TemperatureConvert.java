d9b6c5b4b08b32063bcd3ad8a0650b606045d8b2
FIRDA AMINY MA'RUF <firda_maruf@yahoo.com>
2016-01-19 05:07:30 +0700

    Update TemperatureConvert.java

--- TemperatureConvert.java
+++ TemperatureConvert.java
@@ -1,5 +1,10 @@
 //Firda Aminy Ma'ruf 1301144311 IF3801
-package temperatureconverter;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperatureconverternew;
 
 import java.util.Scanner;
 
@@ -7,43 +12,163 @@ import java.util.Scanner;
  *
  * @author Iwan Ma'ruf
  */
-public class TemperatureConverter {
+public class TemperatureConverterNew {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        int pil, cel;
+        double fah, rea, kel;
+        
         Scanner celcius=new Scanner(System.in);
         Scanner pilihan=new Scanner(System.in);
         System.out.print("Please enter the Celcius degree: ");
-        int cel=celcius.nextInt();
-        System.out.println("Convert the Celcius into: ");
-        System.out.println("1. Fahrenheit");
-        System.out.println("2. Reamur");
-        System.out.println("3. Kelvin");
-        System.out.print("Enter the menu number that you choose: ");
-        int pil=pilihan.nextInt();
-        switch(pil) {
+        cel=celcius.nextInt();
+        do {
+            System.out.println("Convert the Celcius into: ");
+            System.out.println("1. Fahrenheit");
+            System.out.println("2. Reaumur");
+            System.out.println("3. Kelvin");
+            System.out.print("Enter the menu number that you choose: ");
+            pil=pilihan.nextInt();
+            switch(pil) {
             case 1:
             {
-                double fah=((cel*9/5)+32);
+                fah=((cel*9/5)+32);
                 System.out.println("The result of the convert is " +fah+ " Fahrenheit");
                 break;
             }
             case 2:
             {
-                double rea=(cel*4/5);
-                System.out.println("The result of the convert is " +rea+ " Reamur");
+                rea=(cel*4/5);
+                System.out.println("The result of the convert is " +rea+ " Reaumur");
                 break;
             }
             case 3:
             {
-                double kel=(cel+273);
+                kel=(cel+273);
                 System.out.println("The result of the convert is " +kel+ " Kelvin");
                 break;
             }
-            default:
-                throw new AssertionError();
-        }
+            }
+        } while (pil < 3);
     }
+    
+}
+
+       /*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperatureconverternew;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Iwan Ma'ruf
+ */
+public class TemperatureConverterNew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int pil, cel;
+        double fah, rea, kel;
+        
+        Scanner celcius=new Scanner(System.in);
+        Scanner pilihan=new Scanner(System.in);
+        System.out.print("Please enter the Celcius degree: ");
+        cel=celcius.nextInt();
+        do {
+            System.out.println("Convert the Celcius into: ");
+            System.out.println("1. Fahrenheit");
+            System.out.println("2. Reaumur");
+            System.out.println("3. Kelvin");
+            System.out.print("Enter the menu number that you choose: ");
+            pil=pilihan.nextInt();
+            switch(pil) {
+            case 1:
+            {
+                fah=((cel*9/5)+32);
+                System.out.println("The result of the convert is " +fah+ " Fahrenheit");
+                break;
+            }
+            case 2:
+            {
+                rea=(cel*4/5);
+                System.out.println("The result of the convert is " +rea+ " Reaumur");
+                break;
+            }
+            case 3:
+            {
+                kel=(cel+273);
+                System.out.println("The result of the convert is " +kel+ " Kelvin");
+                break;
+            }
+            }
+        } while (pil < 3);
+    }
+    
+}
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package temperatureconverternew;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Iwan Ma'ruf
+ */
+public class TemperatureConverterNew {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int pil, cel;
+        double fah, rea, kel;
+        
+        Scanner celcius=new Scanner(System.in);
+        Scanner pilihan=new Scanner(System.in);
+        System.out.print("Please enter the Celcius degree: ");
+        cel=celcius.nextInt();
+        do {
+            System.out.println("Convert the Celcius into: ");
+            System.out.println("1. Fahrenheit");
+            System.out.println("2. Reaumur");
+            System.out.println("3. Kelvin");
+            System.out.print("Enter the menu number that you choose: ");
+            pil=pilihan.nextInt();
+            switch(pil) {
+            case 1:
+            {
+                fah=((cel*9/5)+32);
+                System.out.println("The result of the convert is " +fah+ " Fahrenheit");
+                break;
+            }
+            case 2:
+            {
+                rea=(cel*4/5);
+                System.out.println("The result of the convert is " +rea+ " Reaumur");
+                break;
+            }
+            case 3:
+            {
+                kel=(cel+273);
+                System.out.println("The result of the convert is " +kel+ " Kelvin");
+                break;
+            }
+            }
+        } while (pil < 3);
+    }
+    
 }
