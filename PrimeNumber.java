cf2b71d77dd412f6c3fdab5554adb8aa1e80c27f
masruril <frostmorning@ymail.com>
2016-01-18 23:37:02 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,31 @@
+/*
+Andi Masruril Haq
+1301140399
+IF 38-09
+*/
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int bil, i;
+        boolean prima;
+        
+        Scanner inputan= new Scanner (System.in);
+        System.out.print("Masukkan Angka : ");
+        bil = inputan.nextInt();
+        prima = true;
+        for (i=2;i<bil;i++){
+            if ((bil % i) == 0){
+                prima = false;
+            }
+        }
+        if (prima == true)
+            System.out.println(bil+ " adalah bilangan prima");
+        else
+            System.out.println(bil + " adalah bukan bilangan prima");
+        
+    }
+    
+}
