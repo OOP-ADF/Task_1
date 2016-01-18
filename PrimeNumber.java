9e7a96df197f66606ad0daa2c06e5753be206e39
biyanakbar <biyan96akbar@gmail.com>
2016-01-18 11:43:36 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,39 @@
+/*
+Biyan Ilham Akbar
+1301144319
+IF-38-09
+ */
+package primenumber;
+import java.util.Scanner;
+/**
+ *
+ * @author Biyan Ilham Akbar
+ */
+public class PrimeNumber {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int a;
+        int b;
+        int c;
+        System.out.println("Masukkan sebuah angka : ");
+        Scanner inp=new Scanner(System.in);
+        a=inp.nextInt();
+        c=0;
+        for(b=1;b<=a;b++){
+            if (a%b==0){
+                c++;
+            }
+        }
+        if (c==2){
+            System.out.println("Angka Prima");
+        }
+        else {
+            System.out.println("Bukan angka prima");
+        }
+    }
+    
+}
 
