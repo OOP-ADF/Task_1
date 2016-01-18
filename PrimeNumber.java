6549e57a8e70a8146ff16909d93309788be60570
irghifari <irghifari@gmail.com>
2016-01-18 23:32:27 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,10 @@
-
+public class primenumber {
+	public static void main (String [] args)
+	{
+		int x;
+		int z;
+		int cek = 1;
+		Scanner box = new Scanner(System.in);
+		
+		System.out.print("Input Angka: ");
+		x = box.nextInt();
