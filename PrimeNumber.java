ddf8966d4cfd7cad82f33b3feb3a058dade8ef8e
febbyfeysyah <febbyfeysyah@gmail.com>
2016-01-19 18:33:15 +0700

    PrimeNumber.java
    Nama : Febby Febriansyah
    NIM : 1301140371
    Kelas : IF-38-01

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,30 @@
+//Nama 		: Febby Febriansyah
+//NIM		: 1301140371
+//Kelas		: IF-38-01
+//Program	: PrimeNumber.java
 
+import java.util.Scanner;
+
+class PrimeNumber {
+	public static void main(String args[]){
+		int x;
+		boolean prime = true;
+		Scanner in = new Scanner(System.in);
+		System.out.println();
+		System.out.println("Program Cek Bilangan Prima");
+		System.out.println("==========================");
+		System.out.println();
+		System.out.print("Masukkan Angka = ");
+		x = in.nextInt();
+		for(int i=2;i<x;i++){
+			if(x%i == 0){
+				prime = false;
+			}
+		}
+		System.out.println();
+		if(prime == false){
+			System.out.println(x+" Bukan bilangan prima");
+		}
+		else{System.out.println(x+" Adalah bilangan prima");}
+	}
+}
