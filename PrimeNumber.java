1747d027901c7e221f325f692a5eb96b9e1bb56c
friendlysipayung <friendly.1301144001@gmail.com>
2016-01-20 00:09:05 +0700

    PrimeNumber.java
    Friendly Halomoan Sipayung 
    1301144001
    IF-38-01

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,23 @@
+import java.util.Scanner;
 
+public class PrimeNumber {
+    public static void main(String[] args) {               
+        System.out.println("Program Bilangan Prima");
+        System.out.println();
+        Scanner input = new Scanner(System.in);
+        System.out.print("Input Bilangan : ");
+        int bilangan = input.nextInt();
+        int x = 0;
+        for (int i = 1; i < bilangan; i++){
+            if (bilangan % i == 0){
+                x++;
+            }
+        }
+        if (x == 1) {
+            System.out.println(bilangan+" bilangan prima");
+        }
+        else {
+            System.out.println(bilangan+" bukan bilangan prima");
+        }
+    }
+}
