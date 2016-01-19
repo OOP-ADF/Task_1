2a260647675d5db53f098e670c7767949727c838
Hizas Sabilal Rasyad <hizassr@gmail.com>
2016-01-19 23:19:14 +0700

    Update IndexPoint.java

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,19 @@
+import java.util.Scanner;
 
+public class IndexPoint {
+    public static void main(String[] args) {
+        
+        Scanner cin = new Scanner(System.in);
+        double quiz,uts,uas,akhir; String index = "";
+        System.out.print("Nilai Quiz : "); quiz = cin.nextFloat();
+        System.out.print("Nilai UTS  : "); uts = cin.nextFloat();
+        System.out.print("Nilai UAS  : "); uas = cin.nextFloat();
+        akhir = 0.25*quiz + 0.35*uts + 0.4*uas;
+        if((akhir >= 85) && (akhir <= 100)) index = "Excellent";
+        else if((akhir >= 75) && (akhir <= 84)) index = "Very Good";
+        else if((akhir >= 65) && (akhir <= 74)) index = "Good";
+        else if((akhir >= 50) && (akhir <= 64)) index = "Accepted";
+        else if((akhir >= 0) && (akhir <= 49)) index = "Failed";
+        System.out.println("Grade kamu adalah : " + index);
+    }
+}
