f81e321186505a56812f5a710c4477526c28face
Ferawidya Primadevi <ferawidya@gmail.com>
2016-01-19 18:58:24 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,32 @@
+// Ferawidya Pimadevi//
+// 	1301140201 //
+//	IF - 38 - 01 //
+	
 
+import java.util.Scanner;
+public class primenumber{
+	public static void main(String[] args) 
+	{
+		Scanner input = new Scanner(System.in);
+		int angka, prima;
+		
+		System.out.println("Input Angka : ");
+		angka = input.nextInt();
+		
+		prima=0;
+		for (int i=1; i<=angka; i++)
+		{
+			if (angka % i == 0)
+			{
+				prima++;
+			}
+		}
+		if (prima==2) {
+			System.out.println(+angka+ " adalah bilangan prima");
+		}
+		else{
+			System.out.println(+angka+ " bukan bilang prima");
+		}
+		
+	}
+}
