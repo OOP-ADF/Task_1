9d8334c2aa442ceb6b3f86669a6d3d862300640c
Annisa Rizki Pratiwi Wibowo <annisaarizki@yahoo.co.id>
2016-01-19 16:50:42 +0800

    Update IndexPoint.java
    ANNISA RIZKI PRATIWI WIBOWO
    1301144391

--- IndexPoint.java
+++ IndexPoint.java
@@ -3,43 +3,58 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package primenumber;
+
+//ANNISA RIZKI PRATIWI WIBOWO
+//1301144391
+
+package indexpoint;
 import java.util.Scanner;
 
 /**
  *
  * @author ASUS PC
  */
-public class PrimeNumber {
+public class IndexPoint {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Scanner input=new Scanner(System.in);
-        System.out.print("Masukkan Angka: ");
-        int n=input.nextInt();
-        int x=0;
-        if (n == 1) 
+        Scanner uts = new Scanner(System.in);
+        Scanner uas = new Scanner(System.in);
+        Scanner kuis = new Scanner(System.in);
+        
+        System.out.print("Masukkan Nilai UTS: ");
+        int mid = uts.nextInt();
+        System.out.println();
+        System.out.print("Masukkan Nilai UAS: ");
+        int end = uas.nextInt();
+        System.out.println();
+        System.out.print("Masukkan Nilai Kuis: ");
+        int quiz = kuis.nextInt();
+        System.out.println();
+        
+        double na = ((0.35*mid)+(0.40*end)+(0.25*quiz));
+        if ((na >= 85) && (na <= 100))
         {
-            System.out.println("Bukan Bilangan Prima");
+            System.out.println("EXCELLENT");
         }
-        else
+        else if ((na >= 75) && (na <= 84))
         {
-            for (int i=1; i<=n; i++)
-            {
-                if ((n % i) == 0)
-                {
-                    x=x+1;
-                }
-            }
-            if (x <= 2)
-            {
-                System.out.println("Bilangan Prima");
-            }
-            else
-            {
-                System.out.println("Bukan Bilangan Prima");
-            }
+            System.out.println("VERY GOOD");
         }
+        else if ((na >= 65) && (na <= 74))
+        {
+            System.out.println("GOOD");
+        }
+        else if ((na >= 50) && (na <= 64))
+        {
+            System.out.println("ACCEPTED");
+        }
+        else if ((na >= 0) && (na <= 49))
+        {
+            System.out.println("FAILED");
+        }
+    }
+    
 }
