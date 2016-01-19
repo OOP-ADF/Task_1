4b527ce07017ee7ce16ba52ea3577bc7ccd5ab9a
aliyanurr <aliyanurr@gmail.com>
2016-01-19 18:59:14 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,22 @@
+//Nama : Aliya Nur Rezkita
+//NIM : 1301144161
+//Kelas : IF-38-01
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+	public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.print("Masukkan Angka: ");
+        int x=input.nextInt();
+        int a=0;
+        for (int i = 1; i < x; i++) {
+            if(x%i==0)
+                a=a+1;
+        }
+        if(a==1)
+            System.out.println(x + " adalah bilangan Prima");
+        else   
+            System.out.println(x + " bukan bilangan Prima");
+    }
+}
