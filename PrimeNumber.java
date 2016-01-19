6414b2743e23c5d6cf29ac3e091424860131860c
selviwp <selviwidyapraditiap@yahoo.com>
2016-01-19 18:58:47 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,20 @@
+// SELVI WIDYAPRADITIA PANGESTI, 1301144041, IF-38-01
 
+import java.util.Scanner;
+
+public class PrimeNumber {
+ public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.print("Masukkan Angka: ");
+        int x=in.nextInt();
+        int y=0;
+        for (int i = 1; i < x; i++) {
+            if(x%i==0)
+                y=y+1;
+        }
+        if(y==1)
+            System.out.println("Prima");
+        else  
+            System.out.println("Bukan Prima");
+    }
+}
