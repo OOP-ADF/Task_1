a61c6c7b62d296576f97316d17ae0fe7311efe52
Stephanus Prasetyo <stefennins@gmail.com>
2016-01-19 22:24:10 +0700

    Update PrimeNumber.java

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,20 @@
-
+public class BilanganPrima{
+	public static void main(String[] args){
+		int bilangan = 7;
+		boolean prima = true;
+		
+		for (int i=2; i<=(bilangan/2); i++){
+			if ((bilangan%1) == 0){
+				prima = false;
+				break;
+			}
+		}
+		if (prima){
+			System.out.println(bilangan+" merupakan bilangan prima");
+		}
+		else{
+			System.out.println(bilangan+" bukan bilangan prima");
+		}
+			
+	}
+}
