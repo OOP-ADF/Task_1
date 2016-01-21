50f23e1f052ee235ae193d7f1a3ea0f86b7fff75
elsalifah <elsalifah@gmail.com>
2016-01-21 19:44:44 +0700

    Update PrimeNumber.java
    Elsa Alifah Wijaya - 1301144021 - IF 38 01

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,24 @@
-
+//Elsa Alifah Wijaya 1301144021 IF3801
+package primenumber;
+import java.util.Scanner;
+public class PrimeNumber{
+	public static void main(String[] args){
+		int angka;
+		int prima = 1;
+		Scanner input  = new Scanner (System.in);
+		System.out.print("Masukkan angka: ");
+		angka=input.nextInt();
+		for(int b =2;b<angka;b++){
+			if((angka%b)==0{
+				prima=0;
+				break;
+			}
+		}
+		if (prima==1){
+			System.out.println(angka+" adalah bilangan prima");
+		}
+		else{
+			System.out.println(angka+" adalah bilangan bukan prima");
+		}
+	}
+}
