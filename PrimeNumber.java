a0bcc1eb4fe4cdb6a5c8ffe06437fbdb8fd9c31a
sagabadi <aprayitno084@gmail.com>
2017-01-29 16:47:26 +0700

    belajar class & object
    Saya sudah coba

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,23 @@
-
+package Task_1;
+public class PrimeNumber {
+int x;
+    public void cek (int a)
+    {
+        x = 0;
+        for (int y = 1; y <= a; y++)
+        {
+            if (a % y == 0)
+            {
+                x++;
+            }
+        }
+        if (x == 2)
+        {
+            System.out.println ("Prime");
+        }
+        else
+        {
+            System.out.println("Not Prime");
+        }
+    }
+}
