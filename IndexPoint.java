825d5cb4feccf59ddda369a8563c4ae8a1f5461c
ranestari <ranestari10@gmail.com>
2017-01-29 11:53:16 +0700

    indeks
    tes

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,41 @@
-
+//RanestariSastriani 1301154553 IF3907
+import java.util.Scanner;
+public class IndexPoint{
+	
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Double nAkhir;
+		
+		
+		
+		System.out.println("Masukan Nilai UAS   : ");
+		Double nUAS=sc.nextDouble();
+		System.out.println("Masukan Nilai UTS   : ");
+		Double nUTS=sc.nextDouble();
+		System.out.println("Masukan Nilai Kuis  :");
+		Double nKuis = sc.nextDouble();
+		nAkhir=(0.4*nUAS + 0.35*nUTS+0.25*nKuis);
+		System.out.println("Nilai Akhir = "+nAkhir);
+		
+		if (nAkhir>=85 && nAkhir<=100)
+		{
+			System.out.println("Grade : EXCELLENT");
+		}
+		else if (nAkhir>=75 && nAkhir<=84)
+		{
+			System.out.println("Grade : VERY GOOD");
+		}
+		else if (nAkhir>=65 && nAkhir<=74)
+		{
+			System.out.println("Grade : GOOD");
+		}
+		else if (nAkhir>=50 && nAkhir<=64)
+		{
+			System.out.println("Grade : ACCEPTED");
+		}
+		else if (nAkhir<49)
+		{
+			System.out.println("Grade : FAILED");
+		}
+	}
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,26 @@
-
+//Ranestari Sastriani, 1301154553, IF3907
+import java.util.Scanner;
+public class PrimeNumber{
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		Integer hitung=0;
+		
+		System.out.println("masukan angka :");
+		Integer angka =sc.nextInt(); 
+		
+		
+		for (Integer i=2; i<angka;i++)
+		{
+			if ((angka % i)==0)
+			{
+				hitung++;
+			}
+		}
+		if (hitung>0 || angka<2)
+			System.out.println(angka+" bukan bilangan prima");
+		else 
+			System.out.println(angka+"  bilangan prima");
+			
+		
+	}
+}
