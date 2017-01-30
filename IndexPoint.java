98ddf40f6604fc32bf2267a5164abad1fc37c242
Christianjanifer <christiansimanjuntak12@gmail.com>
2017-01-30 10:16:04 +0700

    Hendrik Christian - 1301150021 -IF39-07
    Tugas PBO Selesai

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,66 @@
 
+/*
+ * write a java program that ask user 3 inputs of number that represent mid-term score (UTS), end-term score (UAS), and Quiz score
+ * calculate the final score of 35% mid-term score, 40% end-term score, and 25% Quiz score
+ * determine the grade of the score using these rules, and write the output grade
+ */
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Christian Janifer
+ */
+public class IndexPoint {
+
+    private static int uts,uas,quiz;
+    private static double na;
+    static Scanner in = new Scanner(System.in);
+    
+    public void inputdata(){
+        
+        System.out.print(" Input Nilai UTS : ");
+        uts = in.nextInt();
+        System.out.print(" Input Nilai UAS : ");
+        uas = in.nextInt();
+        System.out.print(" Input Nilai Quiz: ");
+        quiz = in.nextInt();
+    }
+    
+    public void inputnilai(){
+        na = (uts * 0.35) + (uas * 0.4) + (quiz * 0.25);
+        System.out.print(" Nilai Akhir: ");
+        System.out.println(na);
+        
+        if (na>=85 && na<=100)
+        {
+            System.out.println("Excellent");
+        }
+
+        else if (na>=75 && na<=84)
+        {
+            System.out.println("Very Good");
+        }
+
+        else if (na>=65 && na<=74)
+        {
+            System.out.println("Good");
+        }
+
+        else if (na>=50 && na<=64)
+        {
+            System.out.println("Accepted");
+        }
+
+        else if (na>=0 && na<=49)
+        {
+            System.out.println("Failed");
+        }
+    }
+    
+    public static void main(String[] args) {
+       IndexPoint Q = new IndexPoint();
+       Q.inputdata();
+       Q.inputnilai();
+    }   
+}
---
+++ TemperatureConvert.java
@@ -0,0 +1,75 @@
+/*
+ * write a java program that ask user an input of number that represent a temperature in celcius degrees from 
+ * ask the user the choices to convert the temperature to Reamur degrees from, Fahrenheit degrees from, or Kelvin
+ * write the output
+ *ask the user if he/she wants to convert the number to another degrees from
+ */
+
+import java.util.Scanner;
+import static temperatureconvert.TemperatureConvert.in;
+
+/**
+ *
+ * @author Christian Janifer
+ */
+public class TemperatureConvert {
+
+    private static double na;
+    private static int celc,pilih;
+    static Scanner in = new Scanner(System.in);
+    
+    public void Reamur(){
+        na = celc * 4/5;
+        System.out.print(celc + " Celcius: "+ na +" Reamur" );
+    }
+    
+    public void Fahrenheit(){
+        na = celc * 9/5 + 32;
+        System.out.print(celc + " Celcius: "+ na +" Fahrenheit");
+    }
+    
+    public void Kelvin(){
+        na = celc + 273.15;
+        System.out.print(celc + " Celcius: "+ na +" Kelvin");
+    }
+    
+    public void menu(){
+        do{
+            System.out.println("Choose To Convert");
+            System.out.println("1. Celcius To Reamur");
+            System.out.println("2. Celcius To Fahrenheit");
+            System.out.println("3. Celcius To Kelvin");
+            System.out.print("Choose Menu: ");
+            pilih = in.nextInt();
+        }while(pilih == 1 && pilih == 2 && pilih == 3 && pilih > 3);
+    }
+    
+    public static void main(String[] args) {
+        TemperatureConvert T = new TemperatureConvert();
+        System.out.print("Masukkan Suhu: ");
+        celc = in.nextInt();
+      
+        do{
+            System.out.println();
+            T.menu();
+            System.out.println();
+            do{
+                if(pilih == 1){
+                    T.Reamur();
+                    System.out.println();
+                }
+                if(pilih == 2){
+                    T.Fahrenheit();
+                    System.out.println();
+                }
+                if(pilih == 3){
+                    T.Kelvin();
+                    System.out.println();
+                }
+                
+                System.out.println("Apakah anda ingin ke menu convert ? Tekan '1' jika 'IYA' atau '2' jika 'TIDAK' ");
+                pilih = in.nextInt();
+            }while(pilih != 2 && pilih == 1 && pilih > 2);
+        }while(pilih == 1);
+    }
+}
