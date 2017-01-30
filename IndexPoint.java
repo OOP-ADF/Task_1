9529b26dde01576733593653af9a014e7f82fb22
nisanurhardini <nurhardininisa@gmail.com>
2017-01-30 22:40:04 +0700

    1301154544_Nisa Nurhardini_IF3912

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,36 @@
+/**
+* nama : nisa nurhardini
+* nim : 1301154544
+* kelas : if-39-12
+*/
+import java.util.Scanner;
 
+public class IndexPoint{
+	public static void main (String[] args) {
+		Scanner cin = new Scanner (System.in);
+		System.out.print ("Masukkan Nilai UTS : ");
+		int uts = cin.nextInt();
+		System.out.print ("Masukkan Nilai UAS : ");
+		int uas = cin.nextInt();
+		System.out.print ("Masukkan Nilai Kuis : ");
+		int kuis = cin.nextInt();
+		
+		double nakhir = (0.35*uts) + (0.4*uas) + (0.25*kuis);
+		
+		if (nakhir>=85 && nakhir<=100){
+			System.out.print ("Excellent");
+		}
+		else if (nakhir>=75 && nakhir<=84){
+			System.out.print ("Very Good");
+		}
+		else if (nakhir>=65 && nakhir<=74){
+			System.out.print ("Good");
+		}
+		else if (nakhir>=50 && nakhir<=64){
+			System.out.print ("Accepted");
+		}
+		else {
+			System.out.print ("Failed");
+		}
+	}
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,32 @@
+/**
+* nama : nisa nurhardini
+* nim : 1301154544
+* kelas : if-39-12
+*/
 
+import java.util.Scanner;
+
+public class PrimeNumber{
+	public static void main (String[] args){
+		Scanner cin = new Scanner(System.in);
+		
+		System.out.print ("Masukkan Bilangan : ");
+		int bil = cin.nextInt();
+		
+		int n;
+		
+		n=0;
+		
+		for (int i=1; i<=bil; i++){
+			if ((bil%i)==0){
+				n++;
+			}
+		}
+		if (n==2){
+			System.out.print ("Bilangan Prima");
+		}
+		else{
+			System.out.print ("Bukan Bilangan Prima");
+		}
+	}
+}
