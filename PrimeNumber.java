e779dec0dda9a1ce8052688504ca0268bd73239b
Irvan Nur Apendi <irvannurapendi@gmail.com>
2017-02-01 23:38:25 +0700

    Sudah selesai

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,22 @@
-
+public class ProgramPrima {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan sebuah bilangan : ");
+        int bilangan = input.nextInt();
+        int i = 0;
+        for (int j = 1; j < bilangan; j++) {
+            if (bilangan % j == 0) 
+            {
+                i = i+1;
+            }
+        }
+        if (i == 1) 
+        {
+            System.out.println("Bilangan Prima");
+        }
+        else
+        {
+            System.out.println("Bukan Bilangan Prima");
+        }
+    }
+}
