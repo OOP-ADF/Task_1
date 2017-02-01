a58ac487229b725a60c7fa0c10f7b10e05785e21
Herica Bunga M <hericabungamaharani@gmail.com>
2017-02-01 21:32:24 +0700

    1301154572_Herica Bunga Maharani_IF-3912
    task1

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,25 @@
+/**
+ * Nama: Herica Bunga Maharani
+ * Nim: 1301154572
+ * Kelas: IF-39-12
+ */
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+        System.out.print ("Masukkan Bilangan: ");
+        int a = cin.nextInt();
+        int b = 0;
+        for (int i = 1; i <= a; i++) {
+            if (a%i==0)
+                b++; 
+            }
+            if (b==2) {
+                System.out.println ("Bilangan Prima");
+            } else {
+                System.out.println ("Bukan Prima");
+            }
+    }
+  }
 
