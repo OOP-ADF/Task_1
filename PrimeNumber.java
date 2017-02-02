f1f67294b07e04395a1c54ebf9e8d0d5b4a86533
abdulnursahid <abdul.nursahid@gmail.com>
2017-02-02 21:44:15 +0700

    abdulnursahid_1301154385_if39-07
    sudah selesai

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,34 @@
+/*
+* abdulnursahid - 1301154385 - if39-07
+*/
+
+package primenumber;
+
+import java.util.Scanner;
+
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        int bil, i, p ;
+        p = 0;
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Masukan Bilangan : ");
+        bil = cin.nextInt();
+        
+        for (i=1; i <= bil; i++){
+            if(bil % i == 0){
+                p++;
+            }
+        }
+        
+        if ( p == 2){
+            System.out.println(bil + " Merupakan Bilangan Prima");
+        }
+        else if ( p != 2){
+           System.out.println(bil + " Merupakan Tidak Bilangan Prima"); 
+        } 
+            
+    }
+    
+}
 
