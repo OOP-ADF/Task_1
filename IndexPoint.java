1df7d0f6dac0d81d907628b63922b9a1cbf86f11
sylviauun <sylviadwianggraini@gmail.com>
2017-02-02 21:31:56 +0700

    2

--- IndexPoint.java
+++ IndexPoint.java
@@ -2,28 +2,50 @@
 //1301154469
 //if-39-07
 
-package primenumber;
+package IndexPoint;
 import java.util.Scanner;
 
-public class PrimeNumber{
+public class IndexPoint{
     public static void main(String[] args) {
+        String nama;
+        int nilai1, nilai2, nilai3;
+        double uas, uts, quis, nakhir;
+        
         Scanner input = new Scanner(System.in);
-        System.out.println("Masukkan angka : ");
-        int angka = input.nextInt();
-        int i;
-        boolean bilprima = true;
+        System.out.println("Masukkan Nama : ");
+        nama = input.nextLine();
         
-        for(i=2 ; i<angka ; i++){
-            if((angka % i) == 0){
-                bilprima = false;
-            }
+        System.out.println("Masukkan Nilai kuis : ");
+        nilai1 = input.nextInt();
+        
+        System.out.println("Masukkan Nilai uts : ");
+        nilai2 = input.nextInt();
+        
+        System.out.println("Masukkan Nilai UAS : ");
+        nilai3 = input.nextInt();
+        
+        quis = 0.25 * nilai1;
+        uts = 0.35 * nilai2;
+        uas = 0.4 * nilai3;
+        
+        nakhir = quis+uts+uas;
+        System.out.println("Jadi nilai akhirnya adalah : "+nakhir);
+        
+        if(nakhir>=85 && nakhir<=100){
+            System.out.println("Excellent");
         }
-        if(bilprima)
-            System.out.println(angka+"adalah bilangan prima.");
-        
-        else
-            System.out.println(angka+"bukan bilangan prima");
-        
+        else if(nakhir>=75 && nakhir<=84){
+            System.out.println("Very Good");
+        }
+        else if(nakhir>=65 && nakhir<=74){
+            System.out.println("Good");
+        }
+        else if(nakhir>=50 && nakhir<=64){
+            System.out.println("Accepted");
+        }
+        else if(nakhir>=49 && nakhir<=0){
+            System.out.println("Failed");
+        }
     }
 }
-
+        
