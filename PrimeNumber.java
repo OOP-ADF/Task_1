5ea23ed31b9def00df1e36825eb69a41712439a9
Irvan Nur Apendi <irvannurapendi@gmail.com>
2017-02-02 17:08:09 +0700

    Irvan Nur Apendi 1301154208 IF-39-12

---
+++ PrimeNumber.java
@@ -0,0 +1,23 @@
+//Irvan Nur Apendi 1301154208 IF-39-12
+public class ProgramPrima {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan sebuah bilangan : ");
+        int bilangan = input.nextInt();
+        int i = 0;
+        for (int j = 1; j < bilangan; j++) {
+            if (bilangan % j == 0) 
+            {
+                i = i+1;
+            }
+        }
+        if (i == 1) 
+        {
+            System.out.println("Bilangan Prima");
+        }
+        else
+        {
+            System.out.println("Bukan Bilangan Prima");
+        }
+    }
+}
