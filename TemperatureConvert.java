5687a456125734eac385718979132e7984d0e1fc
nafasamuth <nafasa.muth@outlook.com>
2017-02-03 00:20:46 +0800

    completed

--- TemperatureConvert.java
+++ TemperatureConvert.java
@@ -2,7 +2,7 @@
  * Nafasa Muth Ma'nah
  * 1301150441
  */
-package TemperatureConver;
+package TemperatureConvert;
 
 import java.util.Scanner;
 
@@ -10,29 +10,41 @@ public class TemperatureConvert{
     public static void main(String[] args) {
         Scanner konversi = new Scanner(System.in);
         double celcius, reamur, fahrenheit, kelvin;
-        int counter = 0, pilih;
+        int counter = 0, pilih, lagi;
         
         System.out.print("Masukkan Suhu : "); //Suhu dengan derajat celcius
         celcius = konversi.nextDouble();
-
-        System.out.println("1. Reamur");
-        System.out.println("2. Fahrenheit");
-        System.out.println("3. Kelvin");
-        System.out.println("Pilihan : ");
-        pilih = konversi.nextInt();
+        while (counter == 0){
+            System.out.println("1. Reamur");
+            System.out.println("2. Fahrenheit");
+            System.out.println("3. Kelvin");
+            System.out.println("Pilihan : ");
+            pilih = konversi.nextInt();       
             switch(pilih){
                 case 1:
                     reamur = 0.8 * celcius;
-                    System.out.print(reamur + " derajat reamur");
+                    System.out.println(reamur + " derajat reamur");
                     break;
                 case 2:
                     fahrenheit = (1.8 * celcius) + 32;
-                    System.out.print(fahrenheit + " derajat fahrenheit");
+                    System.out.println(fahrenheit + " derajat fahrenheit");
                     break;
                 case 3:
                     kelvin = celcius + 273;
-                    System.out.print(kelvin + " derajat kelvin");
+                    System.out.println(kelvin + " derajat kelvin");
                     break;
             }
+            System.out.println("Apakah Anda Ingin Mengkonversi Suhu Lahi ? ");
+            System.out.println("1. Ya");
+            System.out.println("2. Tidak");
+            lagi = konversi.nextInt();
+            if(lagi == 1){
+                System.out.println(pilih);  
+            }
+            else{
+                System.exit(0);
+            }     
+        }
+        
     }
 }
