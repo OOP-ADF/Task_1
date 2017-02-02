daa861e323e99450693fddec269a5a5d6501bbef
MRPanda <harddisk1945@gmail.com>
2017-02-02 15:44:40 +0700

    Tugas ini ori

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,4 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package indeksnilai;
+
+import java.util.Scanner;
+
 /**
-*
-*
-*/
+ *
+ */
+public class IndeksNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner cin = new Scanner (System.in);
+        System.out.print("Masukkan nilai UTS = ");
+        int uts = cin.nextInt();
+        System.out.print("Masukkan nilai UAS = ");
+        int uas = cin.nextInt();
+        System.out.print("Masukkan nilai Quiz = ");
+        int quiz = cin.nextInt();
+        
+        double nilai = (0.35*uts)+(0.4*uas)+(0.25*quiz);
+        
+            if ((nilai >= 85) & (nilai <= 100)) {
+                System.out.println("Excellent");
+            }
+            else if ((nilai >= 75) & (nilai <= 84)) {
+                System.out.println("Very Good");
+            }
+            else if ((nilai >= 65) & (nilai <= 74)) {
+                System.out.println("Good");
+            }
+            else if ((nilai >= 50) & (nilai <= 64)) {
+                System.out.println("Accepted");
+            }
+            else {
+                System.out.println("Failed");
+            }
+        }
+    }
+    
+
