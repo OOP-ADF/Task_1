aa63a4fcc16d7b17e99ced077276c3fe7e91cd25
Irvan Nur Apendi <irvannurapendi@gmail.com>
2017-02-02 10:11:36 +0700

    Irvan Nur Apendi 1301154208
    IF-39-12

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,33 @@
-
+public class IndexPoint {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan Nilai Quiz : ");
+        int nilaiquiz = input.nextInt();
+        System.out.print("Masukkan Nilai UTS : ");
+        int nilaiuts = input.nextInt();
+        System.out.print("Masukkan nilai UAS : ");
+        int nilaiuas = input.nextInt();
+        
+        double nilaiakhir = ((0.25*nilaiquiz) + (0.35*nilaiuts) + (0.4*nilaiuas));
+        if (nilaiakhir >= 85 & <= 100) 
+        {
+            System.out.print("Grade = Excellent");
+        }
+        else if (nilaiakhir >= 75 & <= 84)
+        {
+            System.out.print("Grade = Very Good");
+        }
+        else if (nilaiakhir >= 65 & <= 74)
+        {
+            System.out.print("Grade = Good");
+        }
+        else if (nilaiakhir >= 50 & <= 64)
+        {
+            System.out.print("Grade = Accepted");
+        }
+        else if (nilaiakhir >= 0 & <= 49)
+        {
+            System.out.print("Grade = Failed");
+        }
+    }
+}
---
+++ TemperatureConvert.java
@@ -0,0 +1,33 @@
+public class TemperatureConvert {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan suhu yang akan di convert : ");
+        int suhu = input.nextInt();
+        System.out.println("Pilih akan di convert ke mana : ");
+        System.out.println("1. Reamur");
+        System.out.println("2. Kelvin");
+        System.out.println("3. Fahrenheit");
+        
+        double hasil;
+        int pilihan = input.nextInt();
+        if (pilihan == 1) 
+        { 
+            hasil = 0.8 * suhu;
+            System.out.print("Hasilnya adalah = " + hasil + "Reamur");
+        }
+        if (pilihan == 2)
+        {
+            hasil = 273 + suhu;
+            System.out.print("Hasilnya adalah = " + hasil + "Kelvin");
+        }
+        if (pilihan == 3) 
+        {
+            hasil = (9/5 * suhu) + 32;
+            System.out.print("Hasilnya adalah = " + hasil + "Fahrenheit");
+    }
+        else 
+        {
+            System.out.print("Menu yang di input tidak ada di menu ");
+        }
+    }
+}
