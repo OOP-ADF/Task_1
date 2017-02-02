4da6aa25123fdfedb6d3612c00f4cdd59a8442db
nafasamuth <nafasa.muth@outlook.com>
2017-02-02 10:16:55 +0800

    2 soal

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,43 @@
+package IndexPoint;
 
+import java.util.Scanner;
+
+public class IndexPoint{
+    public static void main(String[] args, double UAS, double UTS){
+        String nama;
+        int nilai1, nilai2, nilai3;
+        double kuis, uts, uas, na;
+        
+        Scanner a = new Scanner(System.in);
+        
+        System.out.println(" Masukkan Nama Mahasiswa : ");
+        nama = a.nextLine();
+        System.out.println("Masukkan nilai UTS Mahasiswa : ");
+        nilai1 = a.nextInt();
+        System.out.println("Masukkan nilai UAS Mahasiswa : ");
+        nilai2 = a.nextInt();
+        System.out.println("Masukkan nilai Kuis Mahasiswa : ");
+        nilai3 = a.nextInt();
+        
+        uts = 0.35*nilai1;
+        uas = 0.4*nilai2;
+        kuis = 0.25*nilai3;
+        na = uts + uas + kuis;
+        if ((na >=85) && (na<=100)){
+            System.out.println("Grade : Excellent");
+        }
+        else if ((na>=75) && (na<=84)){
+            System.out.println("Grade : Very Good");
+        }
+        else if ((na>=65) && (na<=74)){
+            System.out.println("Grade : Good");
+        }
+        else if ((na>=50) && (na<=64)){
+            System.out.println("Grade : Accepted");
+        }
+        else if ((na>=0) && (na<=49)){
+            System.out.println("Grade : Failed");
+        }
+        System.out.print("Mahasiswa dengan Nama " + nama + " Mendapatkan Nilai Akhir : " + na);
+    }   
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,36 @@
-
+package PrimeNumber;
+import java.util.Scanner;
+public class PrimeNumber{
+    Scanner bilangan = new Scanner(System.in);
+    public boolean isPrime(int x){
+        boolean Prime;
+        int count = 0;
+        for (int i = 0; i>= x; i++){
+            if (x % i == 0){
+                count++;
+            }
+    }
+        if (count != 2){
+            Prime = false;
+        }
+        else {
+            Prime = true;
+        }
+        return Prime;
+    }
+    
+    public static void main(String[] args){
+        Scanner bilangan = new Scanner(System.in);
+        PrimeNumber Prima = new PrimeNumber();
+        
+        int x;
+        System.out.print("Masukkan Bilangan : ");
+        x = bilangan.nextInt();
+        if(Prima.isPrime(x)){
+            System.out.println(x + " merupakan bilangan prima");
+        }
+        else {
+            System.out.println(x + " bukan bilangan prima");
+        }
+    }
+}
