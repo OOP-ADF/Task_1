1c32f61d2d033d575613d294b2933cef6b1de601
nurulevawani <nurulevawani22@gmail.com>
2017-02-02 10:11:20 -0800

    1301154530_Nurul Evawani_IF39-12

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,32 @@
+import java.util.Scanner;
+
+public class IndexPoint {
+    public static void main(String[] args) {
+        System.out.println("NILAI");
+        Scanner cin = new Scanner(System.in);
+        System.out.print("Nilai Kuis: ");
+        int kuis = cin.nextInt();
+        System.out.print("Nilai UTS: ");
+        int uts = cin.nextInt();
+        System.out.print("Nilai UAS: ");
+        int uas = cin.nextInt();
+        
+        double finalscore = ((kuis*0.25) + (uts*0.35) + (uas*0.4));
+        if (finalscore >= 85 && finalscore <= 100) {
+            System.out.println("Grade : Excellent");
+        }
+        else if (finalscore >= 75 && finalscore <= 85) {
+            System.out.println("grade : Very Good");
+        }
+        else if (finalscore >=65 && finalscore <=74 ) {
+            System.out.println("Grade : Good");
+        }
+        else if (finalscore >=50 && finalscore <= 64) {
+            System.out.println("Grade: Accepted");
+        }
+        else if (finalscore >= 49 && finalscore <=0) {
+            System.out.println("Grade : Failed");
+        }
+    }
+}
 
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,16 +1,19 @@
 public class PrimeNumber {
 	public static void main (String[] args) {
-		int angka;
-		boolean prima= false;
-		if (angka >=2) {
-			prima=true;
-			for (int a=2; a < angka; a++) {
-				if (angka % a==0) {
-					prima = false;
-					break;
-				}
-			}
-		}
-		System.out.println((prima==true?angka+" adalah bilangan prima":angka+" bukan termasuk bilangan prima"));
+		System.out.print("Input Bilangan: ");
+        Scanner cin = new Scanner(System.in);
+        int i = cin.nextInt();
+        int counter = 0;
+        for (int j = 2; j < i; j++) {
+            if(i % j == 0){
+                counter++;
+            }
+        }
+        if(i > 0 && counter == 0){
+            System.out.println("Merupakan Bilangan Prima");
+        }
+        else {
+            System.out.println("Bukan Merupakan Bilangan Prima");
+    }
 	}
 }
