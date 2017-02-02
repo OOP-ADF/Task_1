5af7b371998bb3423e25951e39e991f745cdb522
nurulevawani <nurulevawani22@gmail.com>
2017-02-01 18:24:32 -0800

    Nurul Evawani 1301154530 IF39-12_CobaPrima

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,16 @@
-
+public class PrimeNumber {
+	public static void main (String[] args) {
+		int angka;
+		boolean prima= false;
+		if (angka >=2) {
+			prima=true;
+			for (int a=2; a < angka; a++) {
+				if (angka % a==0) {
+					prima = false;
+					break;
+				}
+			}
+		}
+		System.out.println((prima==true?angka+" adalah bilangan prima":angka+" bukan termasuk bilangan prima"));
+	}
+}
