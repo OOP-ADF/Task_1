47f33b26eba83b437b2589966d8c81fda83b1ff5
Nindia Cahyaning Putri <already.nindia@yahoo.com>
2017-02-03 08:37:45 +0700

    Nindia Cahyaning Putri _ 1301154567_ IF3907
    Task1
    PrimeNumber
    IndexPoint
    Temperature Convert

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,27 @@
 
+//Nindia Cahyaning Putri
+//IF39-07
+//1301154567
+
+package primenumber;
+import java.util.Scanner;
+
+public class PrimeNumber {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
+        System.out.print("Masukan sebuah bilangan : ");
+        int bil = input.nextInt ();
+        boolean prima = true;
+	/*Rumus*/
+        for (int i=2 ; i<bil ; i++) {
+            if ((bil % i) == 0) {
+                prima = false;   
+            }  
+        }
+        if (prima)
+            System.out.print(bil+ " bilangan prima ");
+        else
+            System.out.print(bil+ " bukan bilangan prima ");
+        
+            }
+        }
