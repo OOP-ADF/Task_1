dd89e964ed5b24b1c057a0ed057052ec3d2aa3a1
Ressa Firmansyah Fatah <ressa>
2017-02-03 12:50:17 +0700

    Ressa Firmansyah Fatah_1301154105_IF-39-07
    Task_1 selesai

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,35 @@
+package indexpoint;
+import java.util.Scanner;
+/**
+ *
+ * @author  Ressa Firmansyah Fatah
+ *          1301154105
+ *          IF-39-07
+ */
+public class IndexPoint {
+    public static void main(String[] args) {
+        Scanner input = new Scanner (System.in);
 
+        System.out.print("Input nilai uas : ");
+        int uts = input.nextInt();
+        System.out.print("Input nilai uts : ");
+        int uas = input.nextInt();
+        System.out.print("Input nilai quis : ");
+        int quiz = input.nextInt();
+        
+        double nilai = (uts*0.35)+(uas*0.4)+(quiz*0.25);
+        
+        if (nilai >= 0 && nilai < 50)
+            System.out.println("Nilai : " + nilai + ", Grade : Failed");
+        else if (nilai < 65)
+            System.out.println("Nilai : " + nilai + ", Grade : Accepted");
+        else if (nilai < 75)
+            System.out.println("Nilai : " + nilai + ", Grade : Good");
+        else if (nilai < 85)
+            System.out.println("Nilai : " + nilai + ", Grade : Very Good");
+        else if (nilai <= 100)
+            System.out.println("Nilai : " + nilai + ", Grade : Exellent");
+        else
+            System.out.println("error");
+    }   
+}
