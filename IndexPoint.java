3ecf8f90289aca815f787630beb69b680364df47
Ressa Firmansyah Fatah <ressa>
2017-02-03 22:47:25 +0700

    Andre Anugrah_1301154371_IF-39-07
    task finish

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,35 @@
-
+package task_1;
+import java.util.Scanner;
+/**
+ *
+ * @author Andre Anugrah
+ */
+public class IndexPoint 
+{
+    public static void main(String[] args) 
+    {
+        //input
+        Scanner input = new Scanner (System.in);
+        System.out.print("Nilai uas : ");
+        int uts = input.nextInt();
+        System.out.print("Nilai uts : ");
+        int uas = input.nextInt();
+        System.out.print("Nilai quis : ");
+        int quiz = input.nextInt();
+        
+        //program
+        double nilai = (uts*0.35)+(uas*0.4)+(quiz*0.25);
+        
+        //output
+        if (nilai >= 0 && nilai < 50)
+            System.out.println(nilai + ", Grade : Failed");
+        else if (nilai < 65)
+            System.out.println(nilai + ", Grade : Accepted");
+        else if (nilai < 75)
+            System.out.println(nilai + ", Grade : Good");
+        else if (nilai < 85)
+            System.out.println(nilai + ", Grade : Very Good");
+        else if (nilai <= 100)
+            System.out.println(nilai + ", Grade : Exellent");
+    }   
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,30 @@
-
+package task_1;
+import java.util.Scanner;
+/**
+ *
+ * @author Andre Anugrah
+ */
+public class PrimeNumber 
+{
+    public static void main(String[] args)
+    {
+        //input
+        Scanner inp = new Scanner (System.in);
+        System.out.println("Input angka : ");
+        int x = inp.nextInt();
+        
+        //program
+        int count = 0;
+	for (int i = 1; i <= x; i++){
+		if (x % i == 0){
+			count = count + 1;
+		}
+	}
+        
+        //output
+        if (count == 2)
+		System.out.println("Bilangan prima");
+	else
+		System.out.println("Bukan prima");	 
+    }
+}
