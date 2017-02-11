26b24dc72eec89cf6f63d613f5413dcab361b229
nikoonovian <niko.novian@yahoo.co.id>
2017-02-11 14:50:34 +0700

    Niko Novian Pratama - 1301154315
    TASK 1

--- IndexPoint.java
+++ IndexPoint.java
@@ -1,1 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class IndexPoint {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Masukkan nilai UTS : ");
+        int UTS = input.nextInt();
+        System.out.print("Masukkan nilai UAS : ");
+        int UAS = input.nextInt();
+        System.out.print("Masukkan nilai Quiz: ");
+        int Quiz = input.nextInt();
+        double NilaiTotal = 0.35*UTS + 0.4*UAS + 0.25*Quiz;
+        if (NilaiTotal >=85 && NilaiTotal<=100)
+            System.out.println("Excelent");
+        else if(NilaiTotal >=75 && NilaiTotal <=84)
+            System.out.println("Very Good");
+        else if(NilaiTotal >=65 && NilaiTotal <=74)
+            System.out.println("Good");
+        else if(NilaiTotal >=50 && NilaiTotal <= 64)
+            System.out.println("Accepted");
+        else    
+            System.out.println("Failed");
+    }
+}
--- PrimeNumber.java
+++ PrimeNumber.java
@@ -1,1 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task1;
 
+import java.util.Scanner;
+
+/**
+ *
+ * @author ASUS
+ */
+public class PrimeNumber {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Masukkan angka: ");
+        int x = input.nextInt();
+        int y = 0;
+        for (int i = 1;i<=x;i++)
+        {
+            if (x%i==0)
+                y+=1;
+        }
+            
+        if (y==2) 
+            System.out.println("Bilangan Prima");
+        else
+            System.out.println("Bukan bilangan prima");
+        
+        
+    }
+    
+}
